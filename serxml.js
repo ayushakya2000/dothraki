af633fcc676e5f86124fcfd574460be067013ee1
Ayush Shakya <47942362+ayushakya2000@users.noreply.github.com>
2019-03-24 15:42:42 +0530

    fixed errors

--- serxml.js
+++ serxml.js
@@ -1,15 +1,24 @@
-var input=document.getElementById("form").value||"";
-var res=document.getElementById('translation');
+var count=0;
+function run()
+{
 
-var url = 'https://api.funtranslations.com/translate/dothraki.json?text=' + encodeURI(input);
+  var input=document.getElementById("formtext").value||"";
+  var res=document.getElementById('translation');
 
-var xhr=new XMLHttpRequest();
+  console.log("input  "+input);
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       res.innerHTML = JSON.parse(xhr.responseText).contents.translated;
-    }
-};
-xhr.open("GET", url, true);
-xhr.send();
+  var url = 'https://api.funtranslations.com/translate/dothraki.json?text=' + encodeURI(input);
+  var xhr=new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+         // Typical action to be performed when the document is ready:
+         //console.log(xhr.responseText);
+         res.innerHTML = JSON.parse(xhr.responseText).contents.translated;
+      }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+  count++;
+  console.log(count);
+  document.getElementById("times").innerHTML=count;
+}
